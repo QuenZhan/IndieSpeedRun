21795cc9659095040c1d8d6aa5664f587aab4b86
QuenZhan <adeathjohn@gmail.com>
2013-09-07 14:44:53 +0800

    firts commit

---
+++ Assets/HarpoonGun.js
@@ -0,0 +1,34 @@
+﻿function shoot(){
+	// buletSeed.rigidbody.velocity=(Vector3.right+Vector3.up*2)*20;
+	ropeEnd=buletSeed.rigidbody;
+	// buletSeed.rigidbody.velocity=(posTarget-transform.position).normalizednormalized*20;
+	// if(ropeSeed)
+	// Instantiate(BulletHeadSeed);
+}
+
+var buletSeed:GameObject;
+var ropeSeed:GameObject;
+var posTarget=Vector3.zero;
+private var ropeEnd:Rigidbody;
+var ropLength=20;
+var ropeSapce=1.0;
+var canStrech=true;
+function Start () {
+	shoot();
+}
+
+function FixedUpdate () {
+	if(ropLength>10)buletSeed.rigidbody.velocity=Vector3.right*40;
+	dvec=ropeEnd.transform.position-transform.position;
+	if(dvec.magnitude>ropeSapce){
+		ropLength--;
+		joint=Instantiate(ropeSeed).GetComponent("HingeJoint");
+		joint.transform.position=transform.position;
+		joint.transform.LookAt(ropeEnd.transform);
+		joint.connectedBody=ropeEnd;
+		ropeEnd=joint.rigidbody;
+		if(ropLength==0){
+			ropeEnd.rigidbody.constraints=RigidbodyConstraints.FreezePosition;
+		}
+	}
+}
